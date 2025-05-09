abd720c7e72253dd6a1022ec44aa0bb664819bca
Silviu Ifrim <silviu-marian.ifrim@multiverse.io>
2025-05-09 11:51:02 +0300

    Create `FinancialStatement` model.

--- core/models.py
+++ core/models.py
@@ -20,3 +20,24 @@ class Currency(models.Model):
     symbol = models.CharField(max_length=10)
 
 
+class FinancialStatement(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    date_reported = models.DateField()
+    calendar_year = models.IntegerField()
+    period = models.CharField(
+        max_length=10, db_comment='For example: "Q1", "Q2", "Q3", "Q4", "FY"'
+    )
+
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+    revenue = models.DecimalField(max_digits=20, decimal_places=2)
+    net_income = models.DecimalField(max_digits=20, decimal_places=2)
+    gross_profit = models.DecimalField(max_digits=20, decimal_places=2)
+    operating_income = models.DecimalField(max_digits=20, decimal_places=2)
+    income_before_tax = models.DecimalField(max_digits=20, decimal_places=2)
+    operating_expenses = models.DecimalField(max_digits=20, decimal_places=2)
+    cash_and_equivalents = models.DecimalField(max_digits=20, decimal_places=2)
+    research_and_development_expenses = models.DecimalField(
+        max_digits=20, decimal_places=2
+    )
