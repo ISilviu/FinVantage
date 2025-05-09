df540955d70fce33b12458ce706c70164e4b56bf
Silviu Ifrim <silviu-marian.ifrim@multiverse.io>
2025-05-09 12:21:15 +0300

    Add `ApiUsage` model.

--- ingestion/models.py
+++ ingestion/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class ApiUsage(models.Model):
+    date = models.DateField(unique=True)
+    limit_reached = models.BooleanField(default=False)
