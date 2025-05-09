4422d41216929ce5c97fdd0bb46f9ff5d667089a
Silviu Ifrim <silviu-marian.ifrim@multiverse.io>
2025-05-09 11:18:43 +0300

    Create `Company` model.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    symbol = models.CharField(max_length=10, unique=True)
+    cik = models.CharField(max_length=10, unique=True)
+
+    image = models.URLField(blank=True, null=True)
+
+    sector = models.CharField(max_length=100, blank=True, null=True)
+    industry = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    ipo_date = models.DateField(blank=True, null=True)
